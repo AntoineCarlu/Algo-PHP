e6ad72fc1edc525961f10fecb9010ebfc6ef0437
6Yoru6 <carluantoine66@outlook.com>
2023-05-12 16:28:02 +0200

    structure include

--- php/index.php
+++ php/index.php
@@ -1,3 +1,21 @@
-<?php
 
-phpinfo();
+
+<!-- HTML -->
+<!DOCTYPE html>
+<html lang="en">
+<?php include "./includes/head.inc.html"; ?> <!--Head-->
+<body>
+  <!--HEADER-->
+  <?php include "./includes/header.inc.html"; ?> 
+
+  <div class="row">
+    <!--Nav Bar-->
+    <?php include "./includes/ul.inc.php"; ?> 
+    <!--SECTION-->
+    <?php include "./includes/form.inc.html"; ?>
+  </div>
+
+  <!--FOOTER-->
+  <?php include "./includes/footer.inc.html"; ?>
+</body>
+</html>
