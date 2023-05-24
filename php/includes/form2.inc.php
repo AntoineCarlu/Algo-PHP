d929b76e6e80b42a6fe3672287e5691730393f9a
6Yoru6 <carluantoine66@outlook.com>
2023-05-24 11:11:23 +0200

    form2

---
+++ php/includes/form2.inc.php
@@ -0,0 +1,44 @@
+  <div class="card col-md-4 mx-auto my-1 pb-2">
+    <p class="mb-2">Connaissances</p>
+    <div class="mb-1 form-check col-auto form-check-inline">
+      <input class="form-check-input" type="checkbox" name="html" id="html" value="HTML">
+      <label class="form-check-label" for="html">HTML</label>
+    </div>
+    <div class="mb-1 form-check col-auto form-check-inline">
+      <input class="form-check-input" type="checkbox" name="css" id="css" value="CSS">
+      <label class="form-check-label" for="css">CSS</label>
+    </div>
+    <div class="mb-1 form-check col-auto form-check-inline">
+      <input class="form-check-input" type="checkbox" name="javascript" id="js" value="JavaScript">
+      <label class="form-check-label" for="js">JavaScript</label>
+    </div>
+    <div class="mb-1 form-check col-auto form-check-inline">
+      <input class="form-check-input" type="checkbox" name="php" id="php" value="PHP">
+      <label class="form-check-label" for="php">PHP</label>
+    </div>
+    <div class="mb-1 form-check col-auto form-check-inline">
+      <input class="form-check-input" type="checkbox" name="mysql" id="sql" value="MySQL">
+      <label class="form-check-label" for="sql">MySQL</label>
+    </div>
+    <div class="mb-1 form-check col-auto form-check-inline">
+      <input class="form-check-input" type="checkbox" name="bootstrap" id="bt5" value="Bootstrap">
+      <label class="form-check-label" for="bt5">Bootstrap</label>
+    </div>
+    <div class="mb-1 form-check col-auto form-check-inline">
+      <input class="form-check-input" type="checkbox" name="symfony" id="symfony" value="Symfony">
+      <label class="form-check-label" for="symfony">Symfony</label>
+    </div>
+    <div class="mb-1 form-check col-auto form-check-inline">
+      <input class="form-check-input" type="checkbox" name="react" id="react" value="React">
+      <label class="form-check-label" for="react">React</label>
+    </div>
+    <div>
+      <label class="form-text fs-6" for="colorc">Couleur préferée</label><br>
+      <input class="p-1" type="color" name="color" id="colorc">
+    </div>
+    <div>
+      <label class="form-text fs-6" for="ddn">Date de naissance</label><br>
+      <input class="" type="date" name="birthday" id="ddn">
+    </div>
+  </div>
+</div>
--- php/index.php
+++ php/index.php
@@ -6,7 +6,9 @@ if (!empty($_SESSION)) {$table = $_SESSION['table'];}
 <!-- HTML -->
 <!DOCTYPE html>
 <html lang="en">
-<?php include "./includes/head.inc.html"; ?> <!--Head-->
+<!--Head-->
+<?php include "./includes/head.inc.html"; ?>
+
 <body>
   <div class="container-xlg">
     <!--HEADER-->
@@ -14,6 +16,7 @@ if (!empty($_SESSION)) {$table = $_SESSION['table'];}
       <?php include "./includes/header.inc.html"; ?>
     </div> 
 
+    <!--Content-->
     <div class="row justify-content-center mt-5">
       <!--Nav Bar-->
       <nav class="col-md-2">
@@ -26,87 +29,118 @@ if (!empty($_SESSION)) {$table = $_SESSION['table'];}
         </ul>
       </nav>
       <!--SECTION-->
-      <div class="col-md-9">
-      <?php 
-        //Include form when on page ?add
-        if (isset($_GET['add'])) {
-          echo "<h2 class='text-center mb-3'>Ajouter des données</h2>";
-          include "./includes/form.inc.html";
-        }
-        //Define variable "table" when submit form
-        else if (isset($_POST['submitForm'])) {
-          $firstName = $_POST['first-name'];
-          $lastName = $_POST['last-name'];
-          $age = $_POST['age'];
-          $size = $_POST['size'];
-          $civility = $_POST['civility'];
-
-          $table = array(
-            'first-name' => $firstName,
-            'last-name' => $lastName,
-            'age' => $age,
-            'size' => $size,
-            'civility' => $civility,
-          );
-          $_SESSION['table'] = $table;
-            //Alert that the data is defined
-            echo "<p class='p-3 mb-2 bg-success text-green text-center'>Données sauvergardées</p>";
-        }
-        //"index.php?debugging" page
-        else if (isset($_GET['debugging']) && !empty($_SESSION)) {
-          echo "<h2 class='text-center mb-5'>Débogage</h2>";
-          echo "<h3 class='fs-5'>===> Lecture du tableau à l'aide de la fonction print_r()</h3>";
-          echo "<pre>";
-          print_r($table);
-          echo "</pre>";
-        }
-        //"index.php?concatenation" page
-        else if (isset($_GET['concatenation']) && !empty($_SESSION)) {
-          echo "<h2 class='text-center mb-5'>Concaténation</h2>";
-          echo "<h3 class='fs-5 mt-5'>===> Construction d'une phrase avec le contenu du tableau</h3>";
-          echo "<p>". (($table['civility'] == 'man') ? "Mr ":"Mme "). $table['first-name']." ". $table['last-name']. "<br>
-            J'ai ". $table['age']. " ans et je mesure ". $table['size']. "m</p>";
-          echo "<h3 class='fs-5 mt-5'>===> Construction d'une phrase après MAJ du tableau</h3>";
-          echo "<p>". (($table['civility'] == 'man') ? "Mr ":"Mme "). ucfirst($table['first-name'])." ". strtoupper($table['last-name']). "<br>
-            J'ai ". $table['age']. " ans et je mesure ". $table['size']. "m</p>";
-          echo "<h3 class='fs-5 mt-5'>===> Affichage d'une virgule à la place du point pour la taille</h3>";
-          echo "<p>". (($table['civility'] == 'man') ? "Mr ":"Mme "). ucfirst($table['first-name'])." ". strtoupper($table['last-name']). "<br>
-            J'ai ". $table['age']. " ans et je mesure ". str_replace('.', ',', $table['size']). "m</p>";
-        }
-        //"index.php?loop" page
-        else if (isset($_GET['loop']) && !empty($_SESSION)) {
-          echo "<h2 class='text-center mb-5'>Boucle</h2>";
-          echo "<h3 class='fs-5 mt-5'>===> Lecture du tableau à l'aide d'une boucle foreach</h3>";
-          $counter = 0;
-          foreach ($table as $key => $value) {
-            echo "à la ligne n°". $counter++. " correspond la clé \"$key\" et contient \"$value\"<br>";
+      <section class="col-md-9">
+        <?php 
+          //Include form when on page ?add
+          if (isset($_GET['add'])) {
+            echo "<h2 class='text-center mb-3'>Ajouter des données</h2>";
+            include "./includes/form.inc.html";
+              echo "<!--Submit button--> <button type='submit' name='submitForm' class='btn btn-primary'>Enregistrer les données</button> </form>";
           }
-        }
-        //"index.php?function" page
-        else if (isset($_GET['function']) && !empty($_SESSION)) {
-          echo "<h2 class='text-center mb-5'>Fonction</h2>";
-          echo "<h3 class='fs-5 mt-5'>===> J'utilise ma fonction readTable()</h3>";
-          function readTable($table) {
+          //Include second form when on page ?addmore
+          else if (isset($_GET['addmore'])) {
+            echo "<h2 class='text-center mb-3'>Ajouter plus de données</h2>";
+            echo "<div class='row justify-content-center'>";
+              echo "<div class='card col-md-7 mx-auto my-1 py-3'>";
+                include "./includes/form.inc.html";
+              echo "</div>";
+              include "./includes/form2.inc.php";
+                echo "<!--Submit button--> <button type='submit' name='submitForm' class='btn btn-primary'>Enregistrer les données</button> </form>";
+          }
+          //Define variable "table" when submit form
+          else if (isset($_POST['submitForm'])) {
+            $firstName = $_POST['first-name'];
+            $lastName = $_POST['last-name'];
+            $age = $_POST['age'];
+            $size = $_POST['size'];
+            $civility = $_POST['civility'];
+
+            $table = array(
+              'first-name' => $firstName,
+              'last-name' => $lastName,
+              'age' => $age,
+              'size' => $size,
+              'civility' => $civility,
+            );
+            $_SESSION['table'] = $table;
+              //Alert that the data is defined
+              echo "<p class='p-3 mb-2 bg-success text-green text-center'>Données sauvergardées</p>";
+          }
+          else if (isset($_POST['submitForm']) && $_GET['addmore']) {
+            $firstName = $_POST['first-name'];
+            $lastName = $_POST['last-name'];
+            $age = $_POST['age'];
+            $size = $_POST['size'];
+            $civility = $_POST['civility'];
+            $color = $_POST['color'];
+
+            $table = array(
+              'first-name' => $firstName,
+              'last-name' => $lastName,
+              'age' => $age,
+              'size' => $size,
+              'civility' => $civility,
+              'color' => $color,
+            );
+            $_SESSION['table'] = $table;
+              //Alert that the data is defined
+              echo "<p class='p-3 mb-2 bg-success text-green text-center'>Données sauvergardées</p>";
+          }
+          //"index.php?debugging" page
+          else if (isset($_GET['debugging']) && !empty($_SESSION)) {
+            echo "<h2 class='text-center mb-5'>Débogage</h2>";
+            echo "<h3 class='fs-5'>===> Lecture du tableau à l'aide de la fonction print_r()</h3>";
+            echo "<pre>";
+            print_r($table);
+            echo "</pre>";
+          }
+          //"index.php?concatenation" page
+          else if (isset($_GET['concatenation']) && !empty($_SESSION)) {
+            echo "<h2 class='text-center mb-5'>Concaténation</h2>";
+            echo "<h3 class='fs-5 mt-5'>===> Construction d'une phrase avec le contenu du tableau</h3>";
+            echo "<p>". (($table['civility'] == 'man') ? "Mr ":"Mme "). $table['first-name']." ". $table['last-name']. "<br>
+              J'ai ". $table['age']. " ans et je mesure ". $table['size']. "m</p>";
+            echo "<h3 class='fs-5 mt-5'>===> Construction d'une phrase après MAJ du tableau</h3>";
+            echo "<p>". (($table['civility'] == 'man') ? "Mr ":"Mme "). ucfirst($table['first-name'])." ". strtoupper($table['last-name']). "<br>
+              J'ai ". $table['age']. " ans et je mesure ". $table['size']. "m</p>";
+            echo "<h3 class='fs-5 mt-5'>===> Affichage d'une virgule à la place du point pour la taille</h3>";
+            echo "<p>". (($table['civility'] == 'man') ? "Mr ":"Mme "). ucfirst($table['first-name'])." ". strtoupper($table['last-name']). "<br>
+              J'ai ". $table['age']. " ans et je mesure ". str_replace('.', ',', $table['size']). "m</p>";
+          }
+          //"index.php?loop" page
+          else if (isset($_GET['loop']) && !empty($_SESSION)) {
+            echo "<h2 class='text-center mb-5'>Boucle</h2>";
+            echo "<h3 class='fs-5 mt-5'>===> Lecture du tableau à l'aide d'une boucle foreach</h3>";
             $counter = 0;
             foreach ($table as $key => $value) {
               echo "à la ligne n°". $counter++. " correspond la clé \"$key\" et contient \"$value\"<br>";
             }
           }
-          readTable($table);
-        }
-        //"index.php?del" page to delete session and datas
-        else if (isset($_GET['del']) && !empty($_SESSION)) {
-          $_SESSION = array();
-          session_destroy();
-          echo "<p class='p-3 mb-2 bg-success text-green text-center'>Données supprimées</p>";
-        }
-        //Default/Home page
-        else {
-          echo "<a href='index.php?add'><button type='button' class='btn btn-primary mx-1'>Ajouter des données</button></a>";
-          echo "<a href='index.php?addmore'><button type='button' class='btn btn-secondary'>Ajouter plus de données</button></a>";
-        }
-      ?>
-      </div>
+          //"index.php?function" page
+          else if (isset($_GET['function']) && !empty($_SESSION)) {
+            echo "<h2 class='text-center mb-5'>Fonction</h2>";
+            echo "<h3 class='fs-5 mt-5'>===> J'utilise ma fonction readTable()</h3>";
+            function readTable($table) {
+              $counter = 0;
+              foreach ($table as $key => $value) {
+                echo "à la ligne n°". $counter++. " correspond la clé \"$key\" et contient \"$value\"<br>";
+              }
+            }
+            readTable($table);
+          }
+          //"index.php?del" page to delete session and datas
+          else if (isset($_GET['del']) && !empty($_SESSION)) {
+            $_SESSION = array();
+            session_destroy();
+            echo "<p class='p-3 mb-2 bg-success text-green text-center'>Données supprimées</p>";
+          }
+          //Default/Home page
+          else {
+            echo "<a href='index.php?add'><button type='button' class='btn btn-primary mx-1'>Ajouter des données</button></a>";
+            echo "<a href='index.php?addmore'><button type='button' class='btn btn-secondary'>Ajouter plus de données</button></a>";
+          }
+        ?>
+      </section>
     </div>
 
     <!--FOOTER-->
