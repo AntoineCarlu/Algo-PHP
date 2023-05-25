4293bc83b664b80ae7e7173336ef28fc45533fa7
6Yoru6 <carluantoine66@outlook.com>
2023-05-25 10:45:43 +0200

    error alerts

--- php/index.php
+++ php/index.php
@@ -89,32 +89,42 @@ if (!empty($_SESSION)) {$table = $_SESSION['table'];}
             if (!empty($_POST['react'])) {$react = $_POST['react'];} else {$react = "";}
             $color = $_POST['color'];
             $birthday = $_POST['birthday'];
-
-            //
             $img = $_FILES['img'];
                 $fileName = $img['name'];
-                $fileType = $img['type'];
+                $fileType = strtolower($img['type']);
                 $fileTmp = $img['tmp_name'];
                 $fileError = $img['error'];
                 $fileSize = $img['size'];
+                $destination = "./uploaded/". $fileName;
 
-                $destination = "./uploaded/"."img1.png";
-                move_uploaded_file($tmpName, $destination);
+                $extensions = ['image/jpg', 'image/png', 'image/jpeg'];
+                $maxSize = 250000;
 
-                $imageDetails = array(
-                  'name' => $fileName,
-                  'type' => $fileType,
-                  'tmp_name' => $fileTmp,
-                  'error' => $fileError,
-                  'size' => $fileSize,
-                  'path' => $destination,
-                );
-                //
-
-            if (empty($firstName&&$lastName&&$age&&$size&&$civility&&$birthday)) {
-              echo "<p class='p-3 mb-2 alert-danger text-red text-center'>Veuillez remplir tout le formulaire</p>";
+            //Error alert if inputs are empty
+            if (empty($firstName&&$lastName&&$age&&$size&&$civility&&$birthday&&$fileName)) {
+              echo "<p class='p-3 mb-2 alert-danger text-red text-center'>Veuillez remplir tout le formulaire.</p>";
               echo "<a class='btn btn-primary' href='index.php?addmore'>Réessayer</a>";
-            } else if (!empty($firstName&&$lastName&&$age&&$size&&$civility&&$birthday)) {
+            } 
+            //Error alert if type doesn't match
+            else if (!in_array($fileType, $extensions)) {
+              echo "<p class='p-3 mb-2 alert-danger text-red text-center'>Le format de l'image n'est pas valide. Utilisez du '.jpg .png .jpeg'.</p>";
+              echo "<a class='btn btn-primary' href='index.php?addmore'>Réessayer</a>";
+            } 
+            //Error alert if size too high
+            else if ($fileSize >= $maxSize) {
+              echo "<p class='p-3 mb-2 alert-danger text-red text-center'>La taille d'image est trop grande, utilisez une image de 250ko maximum.</p>";
+              echo "<a class='btn btn-primary' href='index.php?addmore'>Réessayer</a>";
+            } 
+            //Error alert if errors are detected
+            else if ($fileError >= 1) {
+              echo "<p class='p-3 mb-2 alert-danger text-red text-center'>$fileError a été détecter lors du téléchargement de l'image.</p>";
+              echo "<a class='btn btn-primary' href='index.php?addmore'>Réessayer</a>";
+            }
+            //Save datas and upload image if everything is ok
+            else if (!empty($firstName&&$lastName&&$age&&$size&&$civility&&$birthday&&$fileName)) {
+              //upload files
+              move_uploaded_file($fileTmp, $destination);
+              //define tables
               $table = array(
                 'first-name' => $firstName,
                 'last-name' => $lastName,
@@ -132,7 +142,15 @@ if (!empty($_SESSION)) {$table = $_SESSION['table'];}
                 'color' => $color,
                 'birthday' => $birthday,
               );
-
+              $imageDetails = array(
+                'name' => $fileName,
+                'type' => $fileType,
+                'tmp_name' => $fileTmp,
+                'error' => $fileError,
+                'size' => $fileSize,
+                'path' => $destination,
+              );
+              $table['image'] = $imageDetails;
               $_SESSION['table'] = $table;
                 //Alert that the data is defined
                 echo "<p class='p-3 mb-2 alert-success text-green text-center'>Données sauvergardées</p>";
@@ -166,9 +184,12 @@ if (!empty($_SESSION)) {$table = $_SESSION['table'];}
           else if (isset($_GET['loop']) && !empty($_SESSION)) {
             echo "<h2 class='text-center mb-5'>Boucle</h2>";
             echo "<h3 class='fs-5 mt-5'>===> Lecture du tableau à l'aide d'une boucle foreach</h3>";
+            //Loop to read datas
             $counter = 0;
+            $img_path = $_SESSION['table']['image']['path'];
             foreach ($table as $key => $value) {
-              echo "à la ligne n°". $counter++. " correspond la clé \"$key\" et contient \"$value\"<br>";
+              if ($key == 'image') {echo "à la ligne n°". $counter++. " correspond la clé \"$key\" et contient <img class='w-75' src='$img_path' alt='balise img'><br>";}
+              else {echo "à la ligne n°". $counter++. " correspond la clé \"$key\" et contient \"$value\"<br>";}
             }
           }
 
@@ -176,10 +197,13 @@ if (!empty($_SESSION)) {$table = $_SESSION['table'];}
           else if (isset($_GET['function']) && !empty($_SESSION)) {
             echo "<h2 class='text-center mb-5'>Fonction</h2>";
             echo "<h3 class='fs-5 mt-5'>===> J'utilise ma fonction readTable()</h3>";
+            //function to read datas
             function readTable($table) {
               $counter = 0;
+              $img_path = $_SESSION['table']['image']['path'];
               foreach ($table as $key => $value) {
-                echo "à la ligne n°". $counter++. " correspond la clé \"$key\" et contient \"$value\"<br>";
+                if ($key == 'image') {echo "à la ligne n°". $counter++. " correspond la clé \"$key\" et contient <img class='w-75' src='$img_path' alt='balise img'><br>";}
+                else {echo "à la ligne n°". $counter++. " correspond la clé \"$key\" et contient \"$value\"<br>";}
               }
             }
             readTable($table);
@@ -187,12 +211,21 @@ if (!empty($_SESSION)) {$table = $_SESSION['table'];}
 
           //"index.php?del" page to delete session and datas
           else if (isset($_GET['del']) && !empty($_SESSION)) {
+            //delete images in local
+            $file_list = glob('./uploaded/*');
+            foreach ($file_list as $image) {
+              if (is_file($image)) {
+                  unlink($image);
+              }
+            }
+            //delete session and datas
             $_SESSION = array();
             session_destroy();
+            //alert datas is destroy
             echo "<p class='p-3 mb-2 alert-success text-green text-center'>Données supprimées</p>";
           }
 
-          //Default/Home page
+          //Default|Home page
           else {
             echo "<a href='index.php?add'><button type='button' class='btn btn-primary mx-1'>Ajouter des données</button></a>";
             echo "<a href='index.php?addmore'><button type='button' class='btn btn-secondary'>Ajouter plus de données</button></a>";
